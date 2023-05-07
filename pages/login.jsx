4a9890eaba0f0cef06d2e17786b1656642e39509
Aurelio <aureliomsn@hotmail.it>
2023-05-07 23:28:42 +0200

    login structure

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,31 +1,50 @@
+import { useState } from 'react';
 import NavBar from './components/navbar';
 import Head from 'next/head'
-import Layout from '../components/layout'
-
-function handleLoginClick() {
-  console.log('a')
-}
+import LoginStyle from '../styles/Login.module.css'
 
 export default function Login() {
+
+  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function updateEmail(event) {
+    setEmail(event.target.value);
+  }
+
+  function updatePassword(event) {
+    setPassword(event.target.value);
+  }
+
+  function handleLoginClick() {
+    console.log(`${email}: ${password}`)
+    if(email != false && password != false) {
+      emailRegex.test(email)
+    }
+    
+  }
+
   return(
-    <Layout>
+    <>
       <Head>
         <title>Login</title>
       </Head>
 
-      <div>
+      <div >
         <NavBar />
-        <form action="" method="post">
-          <input type="email" name="" id="" />
-          <input type="password" name="" id="" />
-          <input type="button" value="Login" onClick={handleLoginClick}/>
-        </form>
+
+        <div className={LoginStyle.loginPage}>
+          <form action="" method="post">
+            <input type="email" name="email" id="" placeholder="email" onKeyUp={updateEmail} />
+            <input type="password" name="password" id="" placeholder="password" onKeyUp={updatePassword} />
+            <input type="button" value="Login" onClick={handleLoginClick}/>
+          </form>
+        </div>
+
       </div>
-      
-    </Layout>
+    </>
+    
   )
 }
-
-// export async function getStaticProps() {
-
-// }
